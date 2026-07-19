c40425f32fac449f740781a59889ef16f615aecd
José Azambuja <joseazambuja548@gmail.com>
2026-07-19 08:41:57 -0300

    suor e lagrimas

--- main.js
+++ main.js
@@ -22,3 +22,19 @@ const observer = new IntersectionObserver((entries) => {
 cards.forEach((card) => {
     observer.observe(card);
 });
+const btnMenu = document.getElementById("btn-menu");
+const menu = document.getElementById("menu-lateral");
+
+console.log(btnMenu);
+console.log(menu);
+
+btnMenu.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("Clique!");
+    menu.classList.toggle("aberto");
+});
+const fecharMenu = document.getElementById("fechar-menu");
+
+fecharMenu.addEventListener("click", () => {
+    menu.classList.remove("aberto");
+});
